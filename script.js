db8af5f5918702325e1a6ea5ab31b8ebc11d6ef1
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-03-15 15:37:15 -0700

    added states and few functions working the logic to display the colors to match

--- script.js
+++ script.js
@@ -20,8 +20,10 @@ const firstBox = document.getElementById('color1');
 const secondBox = document.getElementById('color2');
 const thirdBox = document.getElementById('color3');
 const boxes = document.querySelectorAll('.colorFlash');
-const confirmBtn = document.getElementById('confirm').addEventListener('click', handleConfim);
+const start = document.getElementById('start').addEventListener('click', handleStart)
 const colorButtons = document.querySelectorAll('main > div');
+const confirmBtn = document.getElementById('confirm').addEventListener('click', handleConfim);
+const detonoate = document.getElementById('detonate').addEventListener('click', handleDetonate);
 colorButtons.forEach(function(button){
     button.addEventListener('click', handleClick);
 })
@@ -30,12 +32,62 @@ let randomizedPattern = [];
 let selectedColors = [];
 let currentBox = 0;
 
+let gameOver = false;
+let level1 = false;
+let level2 = false;
+let level3 = false;
+let level4 = false;
+let level5 = false;
+
 function handleClick(e){
     const color = e.target.id
     selectedColors.push(color);
     console.log(selectedColors);
 }
 
+
 function handleConfim(e){
     console.log('Confirming Match!')
+}
+
+function handleDetonate(e){
+    console.log('BOOM! Game over..')
+}
+
+function boxesToBeFilled(cb){
+    boxes.forEach(function(box){
+        box.style.backgroundColor = 'black';
+    })
+    const box = boxes[currentBox];
+    console.log(box);
+    const selected = randomColor();
+    const color = colorList[selected].color
+    box.style.backgroundColor = color
+    randomizedPattern.push(color);
+    console.log(randomizedPattern);
+    console.log(colorList[selected].color);
+    setTimeout(cb, colorList[selected].time);
+}
+
+function fillInTheColors() {
+    if(currentBox < 3){
+        boxesToBeFilled(fillInTheColors);
+        currentBox = ++currentBox;
+    }else{
+        currentBox = 0;
+        boxes.forEach(function(box){
+            box.style.backgroundColor = 'black';
+        })
+    }
+}
+fillInTheColors()
+
+function handleStart(e){
+    console.log('Game Starts NOW!');
+    fillInTheColors();
+}
+
+function randomColor(e){
+    const x = Math.floor(Math.random()*16);
+    return x;
 }
